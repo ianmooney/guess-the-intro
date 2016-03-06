b14af54049198c4490acf9ba0e097ea165ba55dc
ianmooney <ian.mooney@photobox.com>
2016-03-06 17:22:02 +0000

    Add env.ml for handling env vars locally

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Load env variables from config/env.yml
+require 'yaml'
+env_yml = File.expand_path('../env.yml', __FILE__)
+if File.exists?(env_yml)
+  YAML.load_file(env_yml).each do |key,value|
+    ENV[key.to_s] = value
+  end
+end
