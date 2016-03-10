c338f5ec4b30a2a94cc79ccf52a477264d77f031
ianmooney <ian.mooney@photobox.com>
2016-03-10 22:04:45 +0000

    Add logout link

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,7 +1,11 @@
 class SpotifyController < ApplicationController
   def callback
     session[:spotify_auth] = request.env['omniauth.auth']
+    redirect_to root_path
+  end
 
+  def logout
+    session.delete(:spotify_auth)
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'main#index'
 
   get '/auth/spotify/callback', to: 'spotify#callback'
+  get '/logout', to: 'spotify#logout', as: 'logout_spotify'
 end
