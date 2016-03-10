6e4640fe061bc522a1eaf3db9b4394eb48b07328
ianmooney <ian.mooney@photobox.com>
2016-03-10 22:18:23 +0000

    Make sure track has a preview_url

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,13 +3,23 @@ class MainController < ApplicationController
 
   def index
     if spotify_user
-      @track_answer = get_random_track
+      @track_answer = get_answer_track
       @track_options = 4.times.collect { get_random_track }
     end
   end
 
   private
+  def get_answer_track
+    track = get_random_track
+
+    until track.preview_url
+      track = get_random_track
+    end
+
+    track
+  end
+
   def get_random_track
-    spotify_user.playlists.sample.tracks.sample
+    spotify_user.playlists(limit: 50).sample.tracks.sample
   end
 end
